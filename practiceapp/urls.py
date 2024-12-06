7cd41fede2f6d80b506ad8a77fe68215a09eb243
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2024-12-06 11:14:19 +0530

    Add files via upload

--- practiceapp/urls.py
+++ practiceapp/urls.py
@@ -9,6 +9,15 @@ urlpatterns=[
     path('joblist',JobList.as_view()),
     path('viewcountry',views.viewcountry,name='viewcountry'),
     path('viewstate',views.viewstate,name='viewstate'),
-    path('viewcity',views.viewcity,name='viewcity')
+    path('viewcity',views.viewcity,name='viewcity'),
+    path('ajaxload',views.ajaxload,name='ajaxload'),
+    path('ajaxdata',views.ajaxdata,name='ajaxdata'),
+    path('displaycity',views.displaycity,name='displaycity'),
+    path('reg', views.reg, name='reg'),
+    path('loginuser', views.loginuser, name='loginuser'),
+    path('logoutuser', views.logoutuser, name='logoutuser'),
+    path('viewreg', views.viewreg, name='viewreg'),
+    path('createcookie',views.createcookie,name='createcookie'),
+    path('getcookie',views.getcookie,name='getcookie')
 
 ]
--- practiceapp/views.py
+++ practiceapp/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from .models import Job,Country,State,City
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout,login
+from django.http import HttpResponse
+def reg(request):
+    if request.method=='POST':
+        user = User.objects.create_user(request.POST['txtuser'],request.POST['txtemail'],request.POST['txtpass'])
+        user.first_name=request.POST['txtfirst']
+        user.last_name=request.POST['txtlast']
+        user.save()
+        return render(request,"practiceapp/reg.html",{"key":"data inserted successfully"})
+    else:
+        return render(request,"practiceapp/reg.html")
+def loginuser(request):
+    if request.method=='POST':
+        obj = authenticate(username=request.POST['txtemail'],password=request.POST['txtpass'])
+        if obj is not None:
+            login(request,obj)
+            return redirect('/practice/viewreg')
+        else:
+            return render(request,"practiceapp/login.html",{"key":"invalid emailid and password"})
+    else:
+        return render(request,"practiceapp/login.html")
+def logoutuser(request):
+    #del request.session['uid']
+    logout(request)
+    return redirect('/practice/loginuser')
+def viewreg(request):
+    if request.user.is_authenticated:
+      obj = User.objects.all()
+      return render(request,"practiceapp/viewreg.html",{"key":obj})
+    else:
+        return redirect('/practice/loginuser')
+
 class Addition(View):
     def get(self,request):
         return render(request,"practiceapp/add.html")
@@ -40,3 +74,21 @@ def viewcity(request):
     state=State.objects.get(pk=request.GET["q"])
     ctdata = state.city_set.all()
     return render(request,"practiceapp/viewcity.html",{"ctkey":ctdata})
+def ajaxload(request):
+    return render(request,"practiceapp/search.html")
+def ajaxdata(request):
+    data = request.GET["q"]
+    res=State.objects.filter(sname__contains=data)
+    return render(request,"practiceapp/ajaxdata.html",{"key":res})
+def displaycity(request):
+    data=request.GET["q"]
+    res = City.objects.filter(city_id=data)
+    return render(request,"practiceapp/citydata.html",{"key":res})
+
+def createcookie(request):
+    response = HttpResponse("Cookie Set")
+    response.set_cookie('ckey', 'hello')
+    return response
+def getcookie(request):
+    a  = request.COOKIES['ckey']
+    return HttpResponse("value is "+  a);
